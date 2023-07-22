b0aaf814b6843eea59a666ce9ab14fc7d6c2233c
Ananta Poudel <ananta@resimator.fi>
2023-07-23 00:16:03 +0545

    V1: User list page design part completed

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserModel } from 'src/app/shared/models/user.model';
 
 @Component({
-  selector: 'app-user-list',
+  selector: 'um-user-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user-list.component.html',
 })
-export class UserListComponent {}
+export class UserListComponent {
+  mockUsers: UserModel[] = [
+    {
+      id: 1,
+      firstName: 'Lara',
+      lastName: 'Dutta',
+      isAdmin: false,
+      userName: 'lara550',
+      department: 'Management',
+    },
+    {
+      id: 2,
+      firstName: 'Alan',
+      lastName: 'Hinder',
+      isAdmin: true,
+      userName: 'alan550',
+      department: 'Maintenance',
+    },
+  ];
+}
