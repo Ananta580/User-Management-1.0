e56b4d569865a9ada22c94d38319a104e67f780b
Ananta Poudel <ananta@resimator.fi>
2023-07-22 23:28:32 +0545

    V1: User detail page initial setup with basic routing and architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/user-list/user-list.component').then(
+        (c) => c.UserListComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
