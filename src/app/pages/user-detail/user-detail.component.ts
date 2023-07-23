a4fce214202f4ca073c00e2c927e1b2e27c683c0
ananta-poudel <ananta@resimator.fi>
2023-07-24 01:08:38 +0545

    V1: User detial component design and implementation completed

---
+++ src/app/pages/user-detail/user-detail.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { UserModel } from 'src/app/shared/models/user.model';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { UserStore } from 'src/app/store/user.store';
+
+@Component({
+  selector: 'um-user-detail',
+  templateUrl: './user-detail.component.html',
+  standalone: true,
+  imports: [CommonModule, SharedModule, RouterModule],
+})
+export class UserDetailComponent {
+  userId: string = '';
+  user$: Observable<UserModel | undefined> = new Observable();
+  constructor(private _route: ActivatedRoute, private userStore: UserStore) {
+    this.userId = this._route.snapshot.paramMap.get('userId') ?? '';
+    if (this.userId) {
+      this.getUser();
+    }
+  }
+
+  getUser() {
+    this.user$ = this.userStore
+      .select((state) => state.userList)
+      .pipe(
+        map((userList) => userList.find((x) => x.id === Number(this.userId)))
+      );
+  }
+}
