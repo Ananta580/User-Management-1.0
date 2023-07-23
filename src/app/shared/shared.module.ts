be8050eb8aa6b45d8011f31adbedd9d4b944bcae
Ananta Poudel <ananta@resimator.fi>
2023-07-23 23:36:54 +0545

    V1: created reusable components: Input, button, Switch

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SwitchComponent } from './components/switch/switch.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [BrowserModule],
+  declarations: [
+    NavBarComponent,
+    ButtonComponent,
+    InputComponent,
+    SwitchComponent,
+  ],
+  imports: [CommonModule],
   providers: [],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, ButtonComponent, InputComponent, SwitchComponent],
 })
 export class SharedModule {}
